ff92c262d5bf0d5251d0f55bf53c8a59a17d6556
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2018-01-13 15:24:05 +0700

    introduction

---
+++ programing/01_inputoutput.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+    return 0;
+}
