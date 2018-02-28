6a7e6ab0b1e60d303cdf1bc7568beb07630eb347
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2018-02-28 23:56:08 +0700

    print pretty

--- programing/25_prettyprint.cc
+++ programing/25_prettyprint.cc
@@ -10,7 +10,20 @@ int main() {
     double A; cin >> A;
     double B; cin >> B;
     double C; cin >> C;
-    
+    cout << resetiosflags(ios::uppercase|ios::scientific);
+    cout << std::setiosflags (ios::left|ios::fixed);
+    cout << setw(0) << std::hex;
+    cout << std::setiosflags (ios::showbase);
+    cout << (unsigned long)A << std::endl;
+
+    cout << resetiosflags(ios::showbase);
+    cout << setw(0xf) << setfill('_')<< std::dec ;
+    cout << setiosflags (ios::showpos) << setprecision(2);
+    cout  << B << endl;
+
+    cout << resetiosflags(ios::fixed|ios::showpos);
+    cout << setiosflags (ios::scientific | ios::uppercase) << setprecision(9);
+    cout << C << std::endl;
   }
   return 0;
 }
